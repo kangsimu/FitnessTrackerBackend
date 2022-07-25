87501fbe84c6a7dbab12024ae22c5f01ebab99e9
Simu <kangtaeam@gmail.com>
2022-07-25 11:32:51 -0400

    getUser function getUserbyUsername

--- db/users.js
+++ db/users.js
@@ -24,15 +24,42 @@ async function createUser({ username, password }) {
 }
 
 async function getUser({ username, password }) {
-
+try {
+  const {
+    rows: [user],
+  } = await client.query(`
+  SELECT id, username   
+  FROM users
+  WHERE username=$1 AND password=$2;
+  `, [username, password]);
+  console.log(user)
+  return user
+} catch (error) {
+  console.error(error)
+}
 }
 
 async function getUserById(userId) {
 
 }
 
-async function getUserByUsername(userName) {
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `,
+      [username]
+    );
 
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = {
