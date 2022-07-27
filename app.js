c0759fde8063fb30442ef1adacd91f8cf4a880cc
rafaelbchs <rafaelbchacins@gmail.com>
2022-07-27 11:10:47 -0500

    tried to start

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-const {router} = require("./api")
-const { client } = require("./db/client");
+const router = require("./api")
 const cors = require('cors')
 
 // Setup your Middleware and API Router here
@@ -10,8 +9,5 @@ app.use(express.json());
 app.use(cors())
 
 app.use("/api", router)
-
-
-client.connect();
 
 module.exports = app;
