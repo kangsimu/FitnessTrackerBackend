03edc97edd662fbdd6850195989a9e68df83fdc7
Simu <kangtaeam@gmail.com>
2022-07-27 21:19:11 -0400

    activities test

--- api/activities.js
+++ api/activities.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {getAllActivities} = require('../db')
+const {getAllActivities, createActivity, getActivityByName} = require('../db')
+const {requireUser} = require('./utils')
 // GET /api/activities/:activityId/routines
 
 // GET /api/activities
@@ -13,6 +14,24 @@ router.get("/", async (req,res,next) => {
     }
 })
 // POST /api/activities
+router.post("/", requireUser, async (req,res,next) => {
+    try {
+        const { name, description } = req.body;
+        const _activity = await getActivityByName(name)
+        if (_activity) {
+            next({
+                name: 'ActivityExistsError',
+                message: `An activity with name ${_activity.name} already exists`,
+                error: 'There was an error'
+            })   
+        } 
+        const activityObj = {name:name, description:description} 
+        const activity = await createActivity(activityObj)
+        res.send(activity)
+    } catch ({ name, message }) {
+        next({ name, message, status: 401 });
+    }
+}) 
 
 // PATCH /api/activities/:activityId
 
