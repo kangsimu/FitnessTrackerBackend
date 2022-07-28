6c413301f08dc57d7cad657a57a4f48ebd84093d
Simu <kangtaeam@gmail.com>
2022-07-28 11:11:55 -0400

    passing routineId test

--- api/routines.js
+++ api/routines.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getAllPublicRoutines, createRoutine} = require('../db')
+const {getAllPublicRoutines, createRoutine, getRoutineById, updateRoutine} = require('../db')
 const {requireUser} = require('./utils')
 
 
@@ -29,6 +29,27 @@ router.post("/", requireUser, async (req,res,next) => {
 })
 
 // PATCH /api/routines/:routineId
+router.patch("/:routineId", requireUser, async (req,res,next) => {
+    const userId = req.user.id
+    const routineId = Number(req.params.routineId)
+    const { isPublic, name, goal } = req.body;
+    const obj = {id:routineId, isPublic:isPublic, name:name, goal:goal }
+    try {
+      const _routine = await getRoutineById(routineId)
+      if (_routine.creatorId != userId) {
+        res.status(403)
+        next({
+            name: 'You are not the Owner',
+            message: `User ${req.user.username} is not allowed to update ${_routine.name}`,
+            error: 'There was an error'            
+        })
+      }
+      const newRoutine = await updateRoutine(obj)
+      res.send(newRoutine)
+    } catch ({ name, message }) {
+        next({ name, message, status: 401 });
+    }
+})
 
 // DELETE /api/routines/:routineId
 
