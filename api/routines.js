22b033f994c2ca5841f5ed00cacce716091f7bef
Simu <kangtaeam@gmail.com>
2022-07-28 10:37:38 -0400

    get all public routines

--- api/routines.js
+++ api/routines.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+const {getAllPublicRoutines} = require('../db')
 // GET /api/routines
+router.get("/", async (req,res,next) => {
+    try {
+      const routines = await getAllPublicRoutines()
+      res.send(routines)
+    } catch ({ name, message }) {
+        next({ name, message, status: 401 });
+    }
+})
 
 // POST /api/routines
 
