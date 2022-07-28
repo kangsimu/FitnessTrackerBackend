66027ca9a875deceadde4d43c2074fb243443f28
Simu <kangtaeam@gmail.com>
2022-07-28 10:47:38 -0400

    post routines test

--- api/routines.js
+++ api/routines.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {getAllPublicRoutines} = require('../db')
+const {getAllPublicRoutines, createRoutine} = require('../db')
+const {requireUser} = require('./utils')
+
+
 // GET /api/routines
 router.get("/", async (req,res,next) => {
     try {
@@ -12,6 +15,18 @@ router.get("/", async (req,res,next) => {
 })
 
 // POST /api/routines
+router.post("/", requireUser, async (req,res,next) => {
+    const userId = req.user.id
+    const {isPublic, name, goal} = req.body
+    const obj = {creatorId:userId, isPublic:isPublic, name:name, goal:goal}
+    try {
+        const newRoutine = await createRoutine(obj)  
+        res.send(newRoutine)
+    } catch ({ name, message }) {
+        next({ name, message, status: 401 });
+    }
+    
+})
 
 // PATCH /api/routines/:routineId
 
