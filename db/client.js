9dba5a8fdcf94a11a62a0c86faaeeea78afe0d4f
rafaelbchs <rafaelbchacins@gmail.com>
2022-07-28 14:15:31 -0500

    Testing deployment

--- db/client.js
+++ db/client.js
@@ -3,12 +3,6 @@ const { Pool } = require("pg");
 const connectionString =
   process.env.DATABASE_URL || "https://localhost:5432/fitness-dev";
 
-const client = new Pool({
-  connectionString,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : undefined,
-});
+  const client = new Pool(connectionString);
 
 module.exports = client;
